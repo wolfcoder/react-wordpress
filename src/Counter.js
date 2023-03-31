2a392a2abb4da6658486f9d943203dba4108ddc5
wolfcoder <bbg2021@gmail.com>
2023-03-31 09:43:29 +0800

    addding parseInt localStorage

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,11 @@ import React from 'react';
 export default function Counter(){
   const  [count, setCount] = React.useState(0);
 
+  React.useEffect(()=> {
+    if(localStorage.getItem('count')){
+      setCount(parseInt(localStorage.getItem('count')));
+    }
+  }, []);
   React.useEffect(() => {
     localStorage.setItem('count', count)
   },  [count] );
